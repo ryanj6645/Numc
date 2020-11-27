2514cb7db4c56763c0948a099d9a23c2f4b54e00
Jai Sharma <jaisharma@berkeley.edu>
2020-11-27 01:01:02 -0600

    debug

--- unittests/test.py
+++ unittests/test.py
@@ -3,5 +3,8 @@ from unittest import TestCase
 class TestAdd(TestCase):
     def test_small_add(self):
         # TODO: YOUR CODE HERE
-        a = nc.Matrix(3,1)
-        a[0]
+        dp_mat1, nc_mat1 = rand_dp_nc_matrix(2, 2, seed=0)
+        dp_mat2, nc_mat2 = rand_dp_nc_matrix(2, 2, seed=1)
+        is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "add")
+        self.assertTrue(is_correct)
+        print_speedup(speed_up)

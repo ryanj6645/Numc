b805d3b9178da05cf04cd2a1354de06ab83bc876
Jai Sharma <jaisharma@berkeley.edu>
2020-11-27 01:09:12 -0600

    im really getting tired of these ridiculous projects.

--- unittests/test.py
+++ unittests/test.py
@@ -1,10 +1,16 @@
 from utils import *
 from unittest import TestCase
-class TestAdd(TestCase):
-    def test_small_add(self):
-        # TODO: YOUR CODE HERE
-        dp_mat1, nc_mat1 = rand_dp_nc_matrix(2, 2, seed=0)
-        dp_mat2, nc_mat2 = rand_dp_nc_matrix(2, 2, seed=1)
-        is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "add")
-        self.assertTrue(is_correct)
-        print_speedup(speed_up)
+def test_small_add():
+    # TODO: YOUR CODE HERE
+    dp_mat1, nc_mat1 = rand_dp_nc_matrix(2, 2, seed=0)
+    dp_mat2, nc_mat2 = rand_dp_nc_matrix(2, 2, seed=1)
+    is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "add")
+    self.assertTrue(is_correct)
+    print_speedup(speed_up)
+
+def subscript_test():
+    a = nc.Matrix(3, 1)
+    a[0]
+
+subscript_test()
+test_small_add()

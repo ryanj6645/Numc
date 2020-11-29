898d706b86812b6da092a9bf42be4c70ca00ab82
jai sharma <jaisharma@berkeley.edu>
2020-11-29 16:51:01 -0600

    debug

--- unittests/test.py
+++ unittests/test.py
@@ -1,7 +1,7 @@
 from utils import *
 from unittest import TestCase
 
-def test_medium_add(self):
+def test_medium_add():
     dp_mat1, nc_mat1 = rand_dp_nc_matrix(1000, 1000, seed=0)
     dp_mat2, nc_mat2 = rand_dp_nc_matrix(1000, 1000, seed=1)
     nc_mat1 + nc_mat2

a4c5b6c6f032e646399b857df7caf2d692c81d11
jai sharma <jaisharma@berkeley.edu>
2020-11-29 16:32:37 -0600

    debug

--- unittests/test.py
+++ unittests/test.py
@@ -1,47 +1,11 @@
 from utils import *
 from unittest import TestCase
-def test_small_add():
-    # TODO: YOUR CODE HERE
-    dp_mat1, nc_mat1 = rand_dp_nc_matrix(2, 2, seed=0)
-    dp_mat2, nc_mat2 = rand_dp_nc_matrix(2, 2, seed=1)
+
+def test_medium_add(self):
+    dp_mat1, nc_mat1 = rand_dp_nc_matrix(1000, 1000, seed=0)
+    dp_mat2, nc_mat2 = rand_dp_nc_matrix(1000, 1000, seed=1)
+    nc_mat1 + nc_mat2
     is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "add")
-    self.assertTrue(is_correct)
     print_speedup(speed_up)
 
-def subscript_test():
-    a = nc.Matrix(3, 3)
-    a[0]
-
-def subscript_test1():
-    a = nc.Matrix(3, 1)
-    a[0]
-
-def subscript_slice():
-    a = nc.Matrix(3,3)
-    # count = 0
-
-    # for r in range(len(3)):
-    #     for c in range(len(3)):
-    #         a.set(r, c, count)
-    #         count += 1
-
-    a[0,0]
-    a[0:2, 0]
-    a[0, 0:2]
-    a[0:2,0:2]
-
-def subscript_set_slice():
-    a = nc.Matrix(3,3)
-    a[0:2, 0:2] = [[1, 2], [3, 4]]
-
-def powpow():
-    a = nc.Matrix(3,3,1)
-    a.set(1,1,0)
-    a = a ** 3
-
-powpow()
-subscript_set_slice()
-subscript_slice()
-subscript_test1()
-subscript_test()
-test_small_add()
+test_medium_add

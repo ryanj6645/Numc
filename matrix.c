6ea47f38888000f1af21754a6c64be499a833a53
Jai Sharma <jaisharma@berkeley.edu>
2020-11-30 01:23:13 -0600

    debug

--- matrix.c
+++ matrix.c
@@ -497,15 +497,16 @@ int neg_matrix(matrix *result, matrix *mat) {
     // }
     // return 0;
     int cols = mat->cols;
-    //#pragma omp parallel for
+    #pragma omp parallel for
     for (int r = 0; r < mat->rows; r++) {
-        __m256d result1 = _mm256_set1_pd(-1);
-        __m256d result2 = _mm256_set1_pd(-1);
-        __m256d result3 = _mm256_set1_pd(-1);
-        __m256d result4 = _mm256_set1_pd(-1);
-        __m256d result5 = _mm256_set1_pd(-1);
-        __m256d result6 = _mm256_set1_pd(-1);
+
         for(int c = 0; c < cols/24 * 24; c+=24){
+            __m256d result1 = _mm256_set1_pd(-1);
+            __m256d result2 = _mm256_set1_pd(-1);
+            __m256d result3 = _mm256_set1_pd(-1);
+            __m256d result4 = _mm256_set1_pd(-1);
+            __m256d result5 = _mm256_set1_pd(-1);
+            __m256d result6 = _mm256_set1_pd(-1);
             double *temp1 = mat->data[r] + c;
             // m1
             __m256d m1rc1 = _mm256_loadu_pd(temp1);
@@ -554,15 +555,15 @@ int abs_matrix(matrix *result, matrix *mat) {
     // }
     int cols = mat->cols;
     #pragma omp parallel for
-    for (int r = 0; r < mat->rows; r++) {
-        __m256d result1 = _mm256_set1_pd(-1);
-        __m256d result2 = _mm256_set1_pd(-1);
-        __m256d result3 = _mm256_set1_pd(-1);
-        __m256d result4 = _mm256_set1_pd(-1);
-        __m256d result5 = _mm256_set1_pd(-1);
-        __m256d result6 = _mm256_set1_pd(-1);
+    for (int r = 0; r < mat->rows; r++) {    
         for(int c = 0; c < cols/24 * 24; c+=24){
             double *temp1 = mat->data[r] + c;
+            __m256d result1 = _mm256_set1_pd(-1);
+            __m256d result2 = _mm256_set1_pd(-1);
+            __m256d result3 = _mm256_set1_pd(-1);
+            __m256d result4 = _mm256_set1_pd(-1);
+            __m256d result5 = _mm256_set1_pd(-1);
+            __m256d result6 = _mm256_set1_pd(-1);
             // m1
             __m256d m1rc1 = _mm256_loadu_pd(temp1);
 			__m256d m1rc2 = _mm256_loadu_pd(temp1 + 4);

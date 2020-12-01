b28aeb8d0a201467bf4bcf3c3885ce462d17b3e5
Jai Sharma <jaisharma@berkeley.edu>
2020-11-30 23:43:44 -0600

    debug

--- unittests/test.py
+++ unittests/test.py
@@ -8,4 +8,14 @@ def test_medium_add():
     is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "add")
     print_speedup(speed_up)
 
+def mem_test():
+    def f():
+        return nc.Matrix(1000, 1000, 1.58729345)
+
+    a = f()[0:100]
+    b = nc.Matrix(10000, 10000, 2.5)
+    c = nc.Matrix(10000, 10000, 2.5)
+    a[0]
+
+mem_test()
 test_medium_add()

7a3dc6c3c1e49a9fe96b9ead4e69cff6c7681c95
Ryan Johnson <ryanj6645@berkeley.edu>
2020-12-01 15:10:30 -0800

    optimize

--- matrix.c
+++ matrix.c
@@ -540,10 +540,10 @@ int pow_matrix(matrix *result, matrix *mat, int pow) {
             }
         }
     } else if(pow >= 2){
-        // mul_matrix_pow(result, mat, mat);
-        // for (int i = 2; i < pow; i++) {
-        //     mul_matrix_pow(result, result, mat);
-        // }
+        mul_matrix_pow(result, mat, mat);
+        for (int i = 2; i < pow; i++) {
+            mul_matrix_pow(result, result, mat);
+        }
 
         // int num = 2;
         // mul_matrix_pow(result, mat, mat);
@@ -554,12 +554,15 @@ int pow_matrix(matrix *result, matrix *mat, int pow) {
         // for (int i = num; i < pow; i++) {
         //      mul_matrix_pow(result, result, mat);
         // }
-        pow_matrix(result, mat, pow/2);
-        if(pow & 1){
-            mul_matrix_pow(result, result, result);
-            mul_matrix_pow(result, result, mat);
-        }
-        mul_matrix_pow(result, result, result);
+
+
+        //RECURSIVE
+        // pow_matrix(result, mat, pow/2);
+        // if(pow & 1){
+        //     mul_matrix_pow(result, result, result);
+        //     mul_matrix_pow(result, result, mat);
+        // }
+        // mul_matrix_pow(result, result, result);
 
     }
     return 0;

061032e7990718ffee94f52a73368aabda691de4
jai sharma <jaisharma@berkeley.edu>
2020-12-02 01:46:10 -0600

    debug

--- matrix.c
+++ matrix.c
@@ -560,10 +560,9 @@ int mul_matrix_pow(matrix *result, matrix *mat1, matrix *mat2) {
   // AB = C A = 4 * 3 B = 3 * 2 C = 4 * 2
   #pragma omp parallel for
   for (int r = 0; r < temp_m->rows; r++) {
-      for(int i = 0; i < temp_m->cols; i++) {
-
+      for(int c = 0; c < temp_m2->cols; c++){
           double temp = 0;
-          for(int c = 0; c < temp_m2->cols; c++){
+          for(int i = 0; i < temp_m->cols; i++) {
               temp = temp_m->data[r][i] * temp_m2->data[i][c] + temp;
               result->data[r][c] = temp;
           }

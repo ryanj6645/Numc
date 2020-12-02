a3efd82888ee8d07e8482e09d90e45863f7a4752
jai sharma <jaisharma@berkeley.edu>
2020-12-02 02:09:54 -0600

    debug

--- matrix.c
+++ matrix.c
@@ -516,7 +516,7 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
 
 
 
-    #pragma omp parallel for
+    #pragma omp parallel for collapse(2)
     for (int r = 0; r < mat1->rows; r++) {
         #pragma omp parallel for
         for (int i = 0; i < mat1->cols; i++) {

33f342b208697815c04627ad4d51c080381c270c
jai sharma <jaisharma@berkeley.edu>
2020-12-03 01:08:04 -0600

    debug

--- matrix.c
+++ matrix.c
@@ -531,33 +531,6 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
         //     }
         // }
 
-    double* dst = (double*) malloc(mat2->rows * mat2->cols * sizeof(double));
-    int n = mat2->cols;
-    int jump1 = 20;
-    #pragma omp parallel for
-    for(int r = 0; r < mat2->rows; r+= jump1) {
-        for(int c = 0; c < n; c+= jump1) {
-            for(int r2 = r; r2 < jump1 + r; r2++) {
-                for(int c2 = c; c2 < jump1 + c; c2++) {
-                    if (r2 >= mat2->rows || c2 >= n) {
-                        continue;
-                    } else {
-                        dst[r2 + c2 * mat2->rows] = mat2->data2[c2 + r2 * n];
-                    }
-                }
-            }
-        }
-    }
-
-    #pragma omp parallel for
-    for (int r = 0; r < mat1->rows; r++) {
-        for (int i = 0; i < mat1->cols; i++) {
-            for (int c = 0; c < mat2->cols; c++) {
-                result->data[r][c] = mat1->data[r][i] * dst[c * mat2->rows + i] + result->data[r][c];
-            }
-        }
-    }
-
     // double* dst = (double*) malloc(mat2->rows * mat2->cols * sizeof(double));
     // int n = mat2->cols;
     // int jump1 = 20;
@@ -579,19 +552,50 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     // #pragma omp parallel for
     // for (int r = 0; r < mat1->rows; r++) {
     //     for (int i = 0; i < mat1->cols; i++) {
-    //
     //         for (int c = 0; c < mat2->cols; c++) {
-    //             double *temp1 = mat1->data[r] + i;
-    //             double *temp2 = dst + c + i * mat2->rows;
-    //             double *temp3 = result->data[r] + c;
-    //
-    //             __m256d m1rc1 = _mm256_loadu_pd(temp1);
-    //             __m256d m2rc1 = _mm256_loadu_pd(temp2);
-    //             __m256d result1 = _mm256_loadu_pd(temp3);
     //             result->data[r][c] = mat1->data[r][i] * dst[c * mat2->rows + i] + result->data[r][c];
     //         }
     //     }
     // }
+
+    double* dst = (double*) malloc(mat2->rows * mat2->cols * sizeof(double));
+    int n = mat2->cols;
+    int jump1 = 20;
+    #pragma omp parallel for
+    for(int r = 0; r < mat2->rows; r+= jump1) {
+        for(int c = 0; c < n; c+= jump1) {
+            for(int r2 = r; r2 < jump1 + r; r2++) {
+                for(int c2 = c; c2 < jump1 + c; c2++) {
+                    if (r2 >= mat2->rows || c2 >= n) {
+                        continue;
+                    } else {
+                        dst[r2 + c2 * mat2->rows] = mat2->data2[c2 + r2 * n];
+                    }
+                }
+            }
+        }
+    }
+
+    #pragma omp parallel for
+    for (int r = 0; r < mat1->rows; r++) {
+        for (int i = 0; i < mat1->cols; i++) {
+
+            for (int c = 0; c < mat2->cols; c++) {
+                double *temp1 = mat1->data[r] + i;
+                double *temp2 = dst + c + i * mat2->rows;
+                double *temp3 = result->data[r] + c;
+
+                __m256d m1rc1 = _mm256_loadu_pd(temp1);
+                __m256d m2rc1 = _mm256_loadu_pd(temp2);
+                __m256d result1 = _mm256_loadu_pd(temp3);
+
+                result1 = _mm256_fmadd_pd(m1rc1, m2rc1, result1);
+
+                _mm256_storeu_pd(result->data[r] + c, result1)
+                // result->data[r][c] = mat1->data[r][i] * dst[c * mat2->rows + i] + result->data[r][c];
+            }
+        }
+    }
 
 
     // #pragma omp parallel for

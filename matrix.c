cabd7ec41d40ee6f318518092e8803feaf8805bd
jai sharma <jaisharma@berkeley.edu>
2020-12-03 00:39:04 -0600

    debug

--- matrix.c
+++ matrix.c
@@ -547,6 +547,23 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
             }
         }
     }
+    double* dst = (double*) malloc(mat2->rows * mat2->cols * sizeof(double));
+    int n = mat2->cols;
+    int n2 = mat2->rows;
+    int jump1 = 20;
+    for(int r = 0; r < n; r+= jump1) {
+        for(int c = 0; c < n2; c+= jump1) {
+            for(int r2 = r; r2 < jump1 + r; r2++) {
+                for(int c2 = c; c2 < jump1 + c; c2++) {
+                    if (r2 >= mat2->rows || c2 >= n2) {
+                        continue;
+                    } else {
+                        dst[c2 + r2 * n] = mat2->data2[r2 + c2 * n2];
+                    }
+                }
+            }
+        }
+    }
 
     #pragma omp parallel for
     for (int r = 0; r < mat1->rows; r++) {

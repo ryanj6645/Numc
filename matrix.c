08228103424dc9fa569ac34f31923302e81ab203
jai sharma <jaisharma@berkeley.edu>
2020-12-03 00:03:22 -0600

    debug

--- matrix.c
+++ matrix.c
@@ -585,15 +585,18 @@ int mul_matrix_pow(matrix *result, matrix *mat1, matrix *mat2) {
 
 
 
-  #pragma omp parallel for
-  for (int r = 0; r < mat1->rows * mat1->cols; r++) {
-        mat1data[r] = mat1->data[r / mat1->cols][r % mat1->rows];
-  }
-  // memcpy(mat1->data2, temp_m->data2, mat1->rows * mat2->cols * sizeof(double) );
-  #pragma omp parallel for
-  for (int r = 0; r < mat2->rows * mat2->cols; r++) {
-        mat2data[r] = mat2->data[r / mat2->cols][r % mat2->rows];
-  }
+  // #pragma omp parallel for
+  // for (int r = 0; r < mat1->rows * mat1->cols; r++) {
+  //       mat1data[r] = mat1->data[r / mat1->cols][r % mat1->rows];
+  // }
+  //
+  // #pragma omp parallel for
+  // for (int r = 0; r < mat2->rows * mat2->cols; r++) {
+  //       mat2data[r] = mat2->data[r / mat2->cols][r % mat2->rows];
+  // }
+  memcpy(mat1data, mat1->data[0], mat1->rows * mat1->cols);
+  memcpy(mat2data, mat2->data[0], mat2->rows * mat2->cols);
+
   #pragma omp parallel for
   for (int r = 0; r < result->rows * result->cols; r++) {
         result->data[r / result->cols][r % result->rows] = 0;

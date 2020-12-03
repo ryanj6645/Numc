b68584b3689a2b44ce9f4f244cf98e2e17daf464
jai sharma <jaisharma@berkeley.edu>
2020-12-03 00:13:24 -0600

    debug

--- matrix.c
+++ matrix.c
@@ -552,7 +552,7 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     for (int r = 0; r < mat1->rows; r++) {
         for (int i = 0; i < mat1->cols; i++) {
             for (int c = 0; c < mat2->cols; c++) {
-                result->data[r][c] = mat1->data[r][i] * dst[i + c * mat2->rows] + result->data[r][c];
+                result->data[r][c] = mat1->data[r][i] * dst[c + i * mat2->cols] + result->data[r][c];
             }
         }
     }

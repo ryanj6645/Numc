c7e11c2ec59582f943a55604f31a20349fc25ca8
Ryan Johnson <ryanj6645@berkeley.edu>
2020-12-02 17:47:18 -0800

    optimize

--- matrix.c
+++ matrix.c
@@ -533,12 +533,10 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
 
         #pragma omp parallel for
         for (int r = 0; r < mat1->rows; r++) {
-              for (int c = 0; c < mat2->cols; c++) {
-               double temp = 0;
-                for (int i = 0; i < mat1->cols; i++) {
-                  temp  = mat1->data[r][i] * mat2->data[i][c] + temp;
-              }
-              result->data[r][c] = temp;
+            for (int i = 0; i < mat1->cols; i++) {
+                for (int c = 0; c < mat2->cols; c++) {
+                    result->data[r][c] = mat1->data[r][i] * mat2->data[i][c] + result->data[r][c];
+                }
             }
         }
 

ccc348530cc6ce21c6b18ddb44d627eb924f7c60
jai sharma <jaisharma@berkeley.edu>
2020-12-04 02:03:42 -0600

    gogo

--- matrix.c
+++ matrix.c
@@ -687,20 +687,20 @@ int mul_matrix_pow(matrix *result, matrix *mat1, matrix *mat2) {
           mat1t[r][c] = mat1->data[r][c];
         }
     }
+    if (mat1->rows * mat1->cols > 10000) {
+        double ** mat2t = (double **) malloc(mat2->rows * sizeof(double *));
+        double * mat2data = (double *) malloc(mat2->rows * mat2->cols * sizeof(double));
+        for (int i = 0; i < mat2->rows; i++) {
+            mat2t[i] = mat2data + i * mat2->cols;
+        }
 
-    // double ** mat2t = (double **) malloc(mat2->rows * sizeof(double *));
-    // double * mat2data = (double *) malloc(mat2->rows * mat2->cols * sizeof(double));
-    // for (int i = 0; i < mat2->rows; i++) {
-    //     mat2t[i] = mat2data + i * mat2->cols;
-    // }
-    //
-    // #pragma omp parallel for
-    // for (int r = 0; r < mat2->rows; r++) {
-    //     for(int c = 0; c < mat2->cols; c++){
-    //       mat2t[r][c] = mat2->data[r][c];
-    //     }
-    // }
-
+        #pragma omp parallel for
+        for (int r = 0; r < mat2->rows; r++) {
+            for(int c = 0; c < mat2->cols; c++){
+              mat2t[r][c] = mat2->data[r][c];
+            }
+        }
+    }
 
     #pragma omp parallel for
     for (int r = 0; r < result->rows; r++) {
@@ -796,18 +796,18 @@ int mul_matrix_pow(matrix *result, matrix *mat1, matrix *mat2) {
         free(dst);
     }
     else {
-        double ** mat2t = (double **) malloc(mat2->rows * sizeof(double *));
-        double * mat2data = (double *) malloc(mat2->rows * mat2->cols * sizeof(double));
-        for (int i = 0; i < mat2->rows; i++) {
-            mat2t[i] = mat2data + i * mat2->cols;
-        }
-
-        #pragma omp parallel for
-        for (int r = 0; r < mat2->rows; r++) {
-            for(int c = 0; c < mat2->cols; c++){
-              mat2t[r][c] = mat2->data[r][c];
-            }
-        }
+        // double ** mat2t = (double **) malloc(mat2->rows * sizeof(double *));
+        // double * mat2data = (double *) malloc(mat2->rows * mat2->cols * sizeof(double));
+        // for (int i = 0; i < mat2->rows; i++) {
+        //     mat2t[i] = mat2data + i * mat2->cols;
+        // }
+        //
+        // #pragma omp parallel for
+        // for (int r = 0; r < mat2->rows; r++) {
+        //     for(int c = 0; c < mat2->cols; c++){
+        //       mat2t[r][c] = mat2->data[r][c];
+        //     }
+        // }
 
 
         for (int r = 0; r < mat1->rows; r++) {

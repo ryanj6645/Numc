38cb9f8e6dc6f4890da1d64f8ed38777da4ad2e1
jai sharma <jaisharma@berkeley.edu>
2020-12-04 02:30:55 -0600

    gogo

--- matrix.c
+++ matrix.c
@@ -589,7 +589,7 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
                 // double *temp3 = result->data[r] + c;
                 __m256d result1 = _mm256_setzero_pd();
                 int flag = 0;
-                for (int i = 0; i < mat1->cols/48 * 48; i+=48) {
+                for (int i = 0; i < mat1->cols/24 * 24; i+=24) {
                     flag = 1;
                     double *temp1 = mat1->data[r] + i;
                     double *temp2 = dst + i + c * mat2->rows;
@@ -599,12 +599,12 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
                     __m256d m1rc4 = _mm256_loadu_pd(temp1 + 12);
                     __m256d m1rc5 = _mm256_loadu_pd(temp1 + 16);
                     __m256d m1rc6 = _mm256_loadu_pd(temp1 + 20);
-                    __m256d m1rc7 = _mm256_loadu_pd(temp1 + 24);
-                    __m256d m1rc8 = _mm256_loadu_pd(temp1 + 28);
-                    __m256d m1rc9 = _mm256_loadu_pd(temp1 + 32);
-                    __m256d m1rc10 = _mm256_loadu_pd(temp1 + 36);
-                    __m256d m1rc11 = _mm256_loadu_pd(temp1 + 40);
-                    __m256d m1rc12 = _mm256_loadu_pd(temp1 + 44);
+                    // __m256d m1rc7 = _mm256_loadu_pd(temp1 + 24);
+                    // __m256d m1rc8 = _mm256_loadu_pd(temp1 + 28);
+                    // __m256d m1rc9 = _mm256_loadu_pd(temp1 + 32);
+                    // __m256d m1rc10 = _mm256_loadu_pd(temp1 + 36);
+                    // __m256d m1rc11 = _mm256_loadu_pd(temp1 + 40);
+                    // __m256d m1rc12 = _mm256_loadu_pd(temp1 + 44);
 
                     __m256d m2rc1 = _mm256_loadu_pd(temp2);
                     __m256d m2rc2 = _mm256_loadu_pd(temp2 + 4);
@@ -612,12 +612,12 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
                     __m256d m2rc4 = _mm256_loadu_pd(temp2 + 12);
                     __m256d m2rc5 = _mm256_loadu_pd(temp2 + 16);
                     __m256d m2rc6 = _mm256_loadu_pd(temp2 + 20);
-                    __m256d m2rc7 = _mm256_loadu_pd(temp2 + 24);
-                    __m256d m2rc8 = _mm256_loadu_pd(temp2 + 28);
-                    __m256d m2rc9 = _mm256_loadu_pd(temp2 + 32);
-                    __m256d m2rc10 = _mm256_loadu_pd(temp2 + 36);
-                    __m256d m2rc11 = _mm256_loadu_pd(temp2 + 40);
-                    __m256d m2rc12 = _mm256_loadu_pd(temp2 + 44);
+                    // __m256d m2rc7 = _mm256_loadu_pd(temp2 + 24);
+                    // __m256d m2rc8 = _mm256_loadu_pd(temp2 + 28);
+                    // __m256d m2rc9 = _mm256_loadu_pd(temp2 + 32);
+                    // __m256d m2rc10 = _mm256_loadu_pd(temp2 + 36);
+                    // __m256d m2rc11 = _mm256_loadu_pd(temp2 + 40);
+                    // __m256d m2rc12 = _mm256_loadu_pd(temp2 + 44);
 
                     result1 = _mm256_fmadd_pd(m1rc1, m2rc1, result1);
                     result1 = _mm256_fmadd_pd(m1rc2, m2rc2, result1);
@@ -625,12 +625,12 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
                     result1 = _mm256_fmadd_pd(m1rc4, m2rc4, result1);
                     result1 = _mm256_fmadd_pd(m1rc5, m2rc5, result1);
                     result1 = _mm256_fmadd_pd(m1rc6, m2rc6, result1);
-                    result1 = _mm256_fmadd_pd(m1rc7, m2rc7, result1);
-                    result1 = _mm256_fmadd_pd(m1rc8, m2rc8, result1);
-                    result1 = _mm256_fmadd_pd(m1rc9, m2rc9, result1);
-                    result1 = _mm256_fmadd_pd(m1rc10, m2rc10, result1);
-                    result1 = _mm256_fmadd_pd(m1rc11, m2rc11, result1);
-                    result1 = _mm256_fmadd_pd(m1rc12, m2rc12, result1);
+                    // result1 = _mm256_fmadd_pd(m1rc7, m2rc7, result1);
+                    // result1 = _mm256_fmadd_pd(m1rc8, m2rc8, result1);
+                    // result1 = _mm256_fmadd_pd(m1rc9, m2rc9, result1);
+                    // result1 = _mm256_fmadd_pd(m1rc10, m2rc10, result1);
+                    // result1 = _mm256_fmadd_pd(m1rc11, m2rc11, result1);
+                    // result1 = _mm256_fmadd_pd(m1rc12, m2rc12, result1);
 
                     // result->data[r][c] = mat1->data[r][i] * dst[c * mat2->rows + i] + result->data[r][c];
                 }
@@ -642,7 +642,7 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
                     flag = 0;
                 }
 
-                for (int i = mat1->cols/48 * 48; i < mat1->cols; i++) {
+                for (int i = mat1->cols/24 * 24; i < mat1->cols; i++) {
                     result->data[r][c] = mat1->data[r][i] * dst[c * mat2->rows + i] + result->data[r][c];
 
                 }

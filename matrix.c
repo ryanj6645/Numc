bf58f35f8d85e0549fa4a98c7605ff2fbf9b6834
jai sharma <jaisharma@berkeley.edu>
2020-12-04 02:03:52 -0600

    gogo

--- matrix.c
+++ matrix.c
@@ -687,7 +687,7 @@ int mul_matrix_pow(matrix *result, matrix *mat1, matrix *mat2) {
           mat1t[r][c] = mat1->data[r][c];
         }
     }
-    if (mat1->rows * mat1->cols > 10000) {
+    if (mat1->rows * mat1->cols < 10000) {
         double ** mat2t = (double **) malloc(mat2->rows * sizeof(double *));
         double * mat2data = (double *) malloc(mat2->rows * mat2->cols * sizeof(double));
         for (int i = 0; i < mat2->rows; i++) {

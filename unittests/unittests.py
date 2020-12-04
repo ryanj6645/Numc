f4a57c13db69d96572e3c65d149aeaab1cd86396
jai sharma <jaisharma@berkeley.edu>
2020-12-04 00:05:56 -0600

    gogo

--- unittests/unittests.py
+++ unittests/unittests.py
@@ -184,12 +184,7 @@ class TestNeg(TestCase):
 
 
 class TestMul2(TestCase):
-    def test_mul(self):
-        dp_mat1, nc_mat1 = rand_dp_nc_matrix(1764, 1349, seed=0)
-        dp_mat2, nc_mat2 = rand_dp_nc_matrix(1349, 20, seed=1)
-        is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
-        self.assertTrue(is_correct)
-        print_speedup(speed_up)
+
 
 class TestMul(TestCase):
     def test_tiny_mul(self):
@@ -243,6 +238,13 @@ class TestMul(TestCase):
     def test_large_mul(self):
         dp_mat1, nc_mat1 = rand_dp_nc_matrix(2000, 2000, seed=0)
         dp_mat2, nc_mat2 = rand_dp_nc_matrix(2000, 2000, seed=1)
+        is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
+        self.assertTrue(is_correct)
+        print_speedup(speed_up)
+
+    def test_mul(self):
+        dp_mat1, nc_mat1 = rand_dp_nc_matrix(1764, 1349, seed=0)
+        dp_mat2, nc_mat2 = rand_dp_nc_matrix(1349, 20, seed=1)
         is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
         self.assertTrue(is_correct)
         print_speedup(speed_up)
